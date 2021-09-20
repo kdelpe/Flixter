93ceb5b6ec76e2447dab03e32f064b0faa78f3b9
kdelpe <kdelpe@gmail.com>
2021-09-20 02:15:46 -0400

    part 2

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Kervens Delpe on 9/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
