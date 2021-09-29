56714270d11c4ac40cf92d88ab45632196607a65
kdelpe <kdelpe@gmail.com>
2021-09-28 20:21:07 -0400

    bonus part

--- Flixter/MovieGridViewController.swift
+++ Flixter/MovieGridViewController.swift
@@ -70,14 +70,23 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         return cell
     }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-    }
-    */
+        
+        //Find the selected movie
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)
+        let movie = movies[indexPath!.item]
+        
+        //Pass the selected movie to the details view controller
+        let detailViewController = segue.destination as! MovieDetailViewController
+        detailViewController.movie = movie
+        
+        collectionView.deselectItem(at: indexPath!, animated: true)
 
+}
 }
